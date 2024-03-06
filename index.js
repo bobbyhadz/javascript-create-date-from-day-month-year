b922ed3ddddd5640510c791632005da31c0041e2
bobbyhadz <opmftw@gmail.com>
2024-03-06 15:07:28 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// EXAMPLE 1 - Create a Date from day, month, year in JavaScript
+
+const date = new Date(2022, 0, 24);
+
+// 👇️ Mon Jan 24 2022 00:00:00
+console.log(date);
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 2 - Splitting a date string
+
+// const str = '2022-01-24';
+
+// const [year, month, day] = str.split('-');
+
+// const date = new Date(+year, month - 1, day);
+
+// // 👇️ Mon Jan 24 2022 00:00:00
+// console.log(date);
